478ae6debb08949a83bc2e6ee2191bb789eb729e
kumailraza404 <kumailraza404@gmail.com>
2023-01-31 19:05:40 +0000

    added logout functionality on sidebar

--- src/theme/Sidebar/index.tsx
+++ src/theme/Sidebar/index.tsx
@@ -8,49 +8,87 @@ import NightShelterIcon from "@mui/icons-material/NightShelter";
 import WorkIcon from "@mui/icons-material/Work";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import RestaurantIcon from "@mui/icons-material/Restaurant";
+import LogoutIcon from "@mui/icons-material/Logout";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import { Icon, SvgIcon } from "@mui/material";
+import { SvgIcon } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/reducer";
+import { signOut } from "../../store/slice/userSlice";
+import { googleLogout } from "@react-oauth/google";
 
 export default function Sidebar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isSignedIn } = useSelector((state: RootState) => state.user);
   const items = [
     { name: "Home", icon: DashboardIcon, link: "/" },
     { name: "Sleep", icon: NightShelterIcon, link: "/sleep" },
     { name: "Work", icon: WorkIcon, link: "/work" },
     { name: "Fitness", icon: FitnessCenterIcon, link: "/fitness" },
-    { name: "Nutrition", icon: RestaurantIcon, link: "/nutrition" },
     { name: "Happiness", icon: SentimentSatisfiedAltIcon, link: "/happiness" },
     { name: "Wealth", icon: AttachMoneyIcon, link: "/wealth" },
   ];
 
-  const {pathname} = useLocation()
- 
+  const { pathname } = useLocation();
 
+  const logoutUser = () => {
+    dispatch(signOut());
+    googleLogout();
+  };
 
   return (
-
-
     <Drawer>
-      {items.map((item) => {
-        return (
-          <div
-            style={{ marginTop: "25px", 
-            background: pathname === item.link ? "rgba(96.1,96.1,96.1,0.3)" : "transparent", 
-            height:"40px", width:"40px",
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center",
-            borderRadius:"40px",
-            cursor:"pointer"
+      <div>
+        {items.map((item) => {
+          return (
+            <div
+              style={{
+                marginTop: "25px",
+                background:
+                  pathname === item.link
+                    ? "rgba(96.1,96.1,96.1,0.3)"
+                    : "transparent",
+                height: "40px",
+                width: "40px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "40px",
+                cursor: "pointer",
+              }}
+              onClick={() => navigate(item.link)}
+              key={`navlinks_${item.name}`}
+            >
+              <Tooltip title={item.name} placement="right">
+                <SvgIcon component={item.icon} sx={{ color: "#FFFFFF" }} />
+              </Tooltip>
+            </div>
+          );
+        })}
+      </div>
+
+      {isSignedIn && (
+        <div
+          style={{
+            height: "40px",
+            width: "40px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
           }}
-            onClick={() => navigate(item.link)}
-            key={`navlinks_${item.name}`}
-          >
-            <SvgIcon component={item.icon} sx={{ color: "#FFFFFF" }} />
-          </div>
-        );
-      })}
+        >
+          <Tooltip title={"Logout"} placement="right">
+            <SvgIcon
+              component={LogoutIcon}
+              sx={{ color: "#FFFFFF" }}
+              onClick={logoutUser}
+            />
+          </Tooltip>
+        </div>
+      )}
     </Drawer>
   );
 }
--- src/theme/Sidebar/styles.ts
+++ src/theme/Sidebar/styles.ts
@@ -1,19 +1,18 @@
 import { positions } from "@mui/system";
 import { styled } from "@mui/system";
 
-export const Drawer = styled("div")(
-  () => ({
-    // minHeight: "400px",
-    width:"70px",
-    borderRadius:"15px",
-    backgroundColor:"#7164ba",
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-    padding:"50px 5px",
-    height:"85vh",
-    marginTop:"1%",
-    position:"sticky",
-    top:"1%"
-  })
-);
+export const Drawer = styled("div")(() => ({
+  // minHeight: "400px",
+  width: "70px",
+  borderRadius: "15px",
+  backgroundColor: "#7164ba",
+  display: "flex",
+  justifyContent: "space-between",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "50px 5px",
+  height: "85vh",
+  marginTop: "1%",
+  position: "sticky",
+  top: "1%",
+}));
