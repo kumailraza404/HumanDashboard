30f6d106ecefc5682ad92d58a4fc43beba7712b6
kumailraza404 <kumailraza404@gmail.com>
2023-02-02 21:56:53 +0000

    wallet not connected screen

--- src/pages/Wealth/index.tsx
+++ src/pages/Wealth/index.tsx
@@ -1,21 +1,29 @@
 import { Grid } from "@mui/material";
 import { useEffect, useState } from "react";
-import { getCalendarData } from "../../services/calendarServices";
-import { Stack, ToggleButton, ToggleButtonGroup } from "@mui/material";
-import { Text } from "../../styles";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+import { formatAddress, injected } from "../../utils/index";
+import { UserRejectedRequestError } from "@web3-react/injected-connector";
+import { Button, Text } from "../../styles";
 
 const Wealth = () => {
   const [category, setCategory] = useState<string>("work");
 
-  const getCalendarDataForMonth = async () => {
-    const res = await getCalendarData();
-    console.log(res, "Check resss");
-  };
+  const {
+    chainId,
+    account,
+    activate,
+    deactivate,
+    setError,
+    active,
+    library,
+    connector,
+  } = useWeb3React<Web3Provider>();
 
-  useEffect(() => {
-    getCalendarDataForMonth();
-  }, []);
+  useEffect(() => {}, []);
 
+  if (!active) return <ConnectWallet />;
   return (
     <Grid>
       <Grid container display={"flex"} columnSpacing={4}>
@@ -28,3 +36,43 @@ const Wealth = () => {
 };
 
 export default Wealth;
+
+const ConnectWallet = () => {
+  const { chainId, account, activate, setError, active, library, connector } =
+    useWeb3React<Web3Provider>();
+
+  const onClickConnect = () => {
+    activate(
+      injected,
+      (error) => {
+        if (error instanceof UserRejectedRequestError) {
+          // ignore user rejected error
+          console.log("user refused");
+        } else {
+          setError(error);
+        }
+      },
+      false,
+    );
+  };
+
+  useEffect(() => {
+    console.log(chainId, account, active, library, connector);
+  });
+
+  return (
+    <Grid
+      container
+      sx={{ height: "70vh" }}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <Button onClick={onClickConnect}>Connect Wallet</Button>
+      <Text sx={{ marginTop: "20px" }} size={20} weight={600}>
+        Connect your account to continue
+      </Text>
+    </Grid>
+  );
+};
